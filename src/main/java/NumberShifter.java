2060ab8bd2500dd2b356b8d8f4617e78ab345457
CARTER HOVERSTEN <227265@andrews.esc18.net>
2026-03-26 12:01:30 -0500

    Completed Number Shifter

--- src/main/java/NumberShifter.java
+++ src/main/java/NumberShifter.java
@@ -13,9 +13,31 @@ public class NumberShifter
 	public static int[] makeLucky7Array( int size)
 	{
 		//You will need to use Math.random()
-		return null;
+		int[] array = new int[size];
+		for(int i = 0; i< size; i++){
+			int insert = (int)(Math.random()*10);
+			if(insert != 0){
+				array[i] = insert;
+			}
+			else{
+				array[i] = 1;
+			}
+			 
+		}
+		return array;
 	}
 	public static void shiftEm(int[] array)
 	{
+		int storeAdd = 0;
+		int store = 0;
+		for(int i = 0; i< array.length;i++){
+			if(array[i] == 7){
+				store = array[storeAdd];
+				array[storeAdd] = array[i];
+				array[i] = store;
+				storeAdd++;
+			}
+		}
+
 	}
 }
